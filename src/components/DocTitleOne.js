02071fe783f92564cc64c0023adb9f58b76da86b
Sanjeeva <sanjeevapottabathini7729@gmail.com>
2023-08-01 12:49:04 +0530

    custom

---
+++ src/components/DocTitleOne.js
@@ -0,0 +1,13 @@
+import React,{useEffect,useState} from 'react'
+import { useDocumentTitle } from '../hooks/useDocumentTitle'
+
+export const DocTitleOne = () => {
+    const [count,setCount]=useState(0)
+    
+    useDocumentTitle(count)
+    return (
+        <div>
+            <button onClick={()=>setCount(count+1)}>Count - {count}</button>
+        </div>
+    )
+}
---
+++ src/components/UserForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { useInput } from '../hooks/useInput'
+
+export const UserForm = () => {
+    
+    const [firstName,bindFirstName,resetFirstName]=useInput('')
+    const [lastName,bindLastName,resetLastName]=useInput('')
+    const submitHandler=e=>{
+        e.preventDefault()
+        alert(`Hello ${firstName} ${lastName}`)
+        resetFirstName()
+        resetLastName()
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <div>
+                <label>First Name</label>
+                <input type='text' {...bindFirstName}/>
+            </div>
+            <div>
+                <label>Last Name</label>
+                <input type='text' {...bindLastName}/>
+            </div>
+            <button>Submit</button>
+        </form>
+    )
+}
+
